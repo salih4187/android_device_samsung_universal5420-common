2eff72a12d45c7bc3055298a978410857d0a8962
MIUI56 <payush706@gmail.com>
2021-12-02 12:11:38 +0000

    universal5420:Move ueventd....rc to /vendor/etc

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -59,9 +59,9 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.universal5420.rc
-LOCAL_MODULE_STEM   := ueventd.rc
+LOCAL_MODULE_STEM       := ueventd.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/ueventd.universal5420.rc
-LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
