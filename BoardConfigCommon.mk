3ad635e609ed64974a03cb7b40294881a7c2c0a3
garak <elim.garak.dev@gmail.com>
2022-03-11 19:35:07 +0100

    universal5420: kernel build fix after 325948 merge

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -97,6 +97,7 @@ TARGET_LINUX_KERNEL_VERSION := 3.4
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos5420
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_CLANG_COMPILE := false
 
 # Keymaster
 BOARD_USES_TRUST_KEYMASTER := true
