762fffedeb0c95e266813d1ffcc6d16c84e71415
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-10-16 21:12:48 -0600

    universal5420-common: update audio over bluetooth for Android 13.
    Taken from
    
    https://github.com/html6405/android_device_samsung_n7100/commit/a5ff76bb49672e06339d392449838fad315e3353
    https://github.com/html6405/android_device_samsung_smdk4412-common/commit/72cf67ea6c7c0b74f81568e48d7cec225cbdfd8f
    https://github.com/html6405/android_device_samsung_smdk4412-common/commit/1645b3b9c6bffebe471b5d2d26ee8af814f2d6f1
    https://github.com/html6405/android_device_samsung_smdk4412-common/commit/1d4b59c03923f3e993244ad12e2b632155de5098
    
    Same commits work on exynos5433.

--- device-common.mk
+++ device-common.mk
@@ -36,7 +36,9 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     tinymix \
-    android.hardware.audio.service \
+    android.hardware.audio.service.exynos5 \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl
 
@@ -44,7 +46,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(COMMON_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration_7_0.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
@@ -143,7 +146,7 @@ PRODUCT_PACKAGES +=  \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.universal5420
+    android.hardware.power-service.exynos
 
 # Permissions
 PRODUCT_COPY_FILES += \
