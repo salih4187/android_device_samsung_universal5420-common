7078a5d5d1393ef8fccf243085f98559850f3fd0
retiredtab-gerrit <clrh1tablet@gmail.com>
2024-01-09 11:56:49 -0700

    unverisal5420-common: add more missing symbols for n1a devices

--- libshims/libshim_gpsd/libshim_gpsd.cpp
+++ libshims/libshim_gpsd/libshim_gpsd.cpp
@@ -7,6 +7,20 @@ namespace android {
     // android::SensorManager::getSensorList(android::Sensor const* const**) const
     extern "C" void _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE();
     extern "C" void _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(){};
+    extern "C" void _ZN7android9SingletonINS_13SensorManagerEE5sLockE();
+    extern "C" void _ZN7android9SingletonINS_13SensorManagerEE5sLockE(){};
+    extern "C" void _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE();
+    extern "C" void _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE(){};
+    extern "C" void _ZN7android13SensorManagerC1Ev();
+    extern "C" void _ZN7android13SensorManagerC1Ev(){};
+    extern "C" void _ZN7android13SensorManager16createEventQueueEv();
+    extern "C" void _ZN7android13SensorManager16createEventQueueEv(){};
+    extern "C" void _ZN7android8String16aSEOS0_();
+    extern "C" void _ZN7android8String16aSEOS0_(){}
+    extern "C" void _ZN7android8String16C1EOS0_();
+    extern "C" void _ZN7android8String16C1EOS0_(){}
+
+    extern "C" { long (*SSL_CTX_ctrl)(void *ctx, int cmd, long larg, void *parg); }
 
     //android::String16::String16(char const*)
     extern "C" void _ZN7android8String16C1EPKc(void **str16P, const char *str);
