e95b9f96347b3d209dcbe4e5bf79354e479886fa
Bruno Martins <bgcngm@gmail.com>
2024-05-18 19:15:13 -0600

    universal5420-common: Migrate to AIDL ClearKey DRM HAL

--- device-common.mk
+++ device-common.mk
@@ -77,7 +77,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # GNNS
 PRODUCT_PACKAGES += \
