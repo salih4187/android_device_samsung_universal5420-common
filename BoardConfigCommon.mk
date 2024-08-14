4d1cc184068625be8f87c2b756616147e726eed2
HazouPH <jgrharbers@gmail.com>
2024-08-14 22:13:38 +0200

    Camera: Fix Camera recording
    By adding the cameraservice into the mediacodec service
    
    The camera can make the right buffers and the framework can pass them
    around with the right changes. But the OMX can´t get to the buffers,
    because the are in a different process. So merge the two
    
    Change-Id: I7d65e5de8697b8ee71a230da03db5d4f528e3d76

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,9 +39,6 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_HAVE_SAMSUNG_BLUETOOTH := true
 BOARD_BLUEDROID_VENDOR_CONF := $(COMMON_PATH)/bluetooth/libbt_vndcfg.txt
 
-# Camera
-TARGET_HAS_LEGACY_CAMERA_HAL1 := true
-
 # CPU
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -157,8 +154,13 @@ include device/lineage/sepolicy/exynos/sepolicy.mk
 BOARD_SEPOLICY_TEE_FLAVOR := mobicore
 include device/samsung_slsi/sepolicy/sepolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+<<<<<<< HEAD
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 # For legacy HAL1 camera
+=======
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+# For legacy HAL1 camera and more
+>>>>>>> 4f18d19 (Camera: Fix Camera recording)
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Shims
