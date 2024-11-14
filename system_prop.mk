96f545561deb6abd12f269033d78c27e130b4c80
retiredtab-gerrit <you@example.com>
2024-11-13 17:19:47 -0700

    universal5420-common: Disable all stagefright props for QPR3 - otherwise, there's no sound
    AOSP dropped Google codecs in favour of c2 starting with QPR3.

--- system_prop.mk
+++ system_prop.mk
@@ -67,11 +67,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true \
     debug.stagefright.ccodec=0 \
-    debug.stagefright.omx_default_rank=0 \
-    debug.stagefright.omx_default_rank.sw-audio=1
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
+
+# Disable all stagefright for QPR3 - otherwise, there's no sound
+#    debug.stagefright.ccodec=0 \
+#    debug.stagefright.omx_default_rank=0 \
+#    debug.stagefright.omx_default_rank.sw-audio=1
 
 # Network
 # Define default initial receive window size in segments.
