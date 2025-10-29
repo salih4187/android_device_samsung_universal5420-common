4d96441b847e9e85dfd176f3e168b5113a045a29
Salih Çağlar <salihcaglar4187@gmail.com>
2025-10-29 14:44:56 +0300

    universal5420-common: Sync OMX with Arndale Octa Board

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -9,7 +9,7 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CsONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
@@ -130,16 +130,16 @@ BOARD_USE_ANB_OUTBUF_SHARE := true
 BOARD_USE_IMPROVED_BUFFER := true
 BOARD_USE_NON_CACHED_GRAPHICBUFFER := true
 BOARD_USE_GSC_RGB_ENCODER := true
-BOARD_USE_CSC_HW := true
+BOARD_USE_CSC_HW := false
 BOARD_USE_QOS_CTRL := false
 BOARD_USE_S3D_SUPPORT := true
 BOARD_USE_TIMESTAMP_REORDER_SUPPORT := false
 BOARD_USE_DEINTERLACING_SUPPORT := false
 BOARD_USE_VP8ENC_SUPPORT := true
-BOARD_USE_HEVCDEC_SUPPORT := true
-BOARD_USE_HEVCENC_SUPPORT := true
+BOARD_USE_HEVCDEC_SUPPORT := false
+BOARD_USE_HEVCENC_SUPPORT := false
 BOARD_USE_HEVC_HWIP := false
-BOARD_USE_VP9DEC_SUPPORT := true
+BOARD_USE_VP9DEC_SUPPORT := false
 BOARD_USE_VP9ENC_SUPPORT := false
 BOARD_USE_CUSTOM_COMPONENT_SUPPORT := true
 BOARD_USE_VIDEO_EXT_FOR_WFD_HDCP := false
