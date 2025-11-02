10d3419e9056f7f8e8f1d6e84d4eb670156d5876
Salih Çağlar <salihcaglar4187@gmail.com>
2025-11-02 11:15:17 +0300

    universal5420-common: Use AIDL Power HAL

--- device-common.mk
+++ device-common.mk
@@ -150,7 +150,7 @@ PRODUCT_PACKAGES +=  \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.universal5420
+    android.hardware.power-service.exynos
 
 # Permissions
 PRODUCT_COPY_FILES += \
