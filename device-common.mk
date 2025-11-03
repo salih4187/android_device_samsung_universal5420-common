06455b07ea6cae9632d0dde8c866866ac3abbdc6
Salih Çağlar <salihcaglar4187@gmail.com>
2025-11-03 18:35:11 +0300

    universal5420-common: Remove non-existent module

--- device-common.mk
+++ device-common.mk
@@ -144,8 +144,7 @@ PRODUCT_PACKAGES += \
 
 # Network
 PRODUCT_PACKAGES +=  \
-    InProcessNetworkStack \
-    com.android.tethering.inprocess
+    InProcessNetworkStack
 
 # Power
 PRODUCT_PACKAGES += \
