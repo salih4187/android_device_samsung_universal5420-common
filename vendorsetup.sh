88e915edb8eefb529f7fa2bd4f4daffcff162d03
Salih Çağlar <salihcaglar4187@gmail.com>
2025-11-05 15:14:49 +0300

    universal5420: Do not apply video recording patch autimatically

--- vendorsetup.sh
+++
@@ -1,5 +0,0 @@
-echo "Applying: frameworks: av: 008-PATCH-1-2-libstagefright-Support-for-legacy-camera-e.patch"
-patch -d frameworks/av -p1 -N --no-backup-if-mismatch --reject-file=/tmp/rej < device/samsung/universal5420-common/patches/frameworks/av/008-PATCH-1-2-libstagefright-Support-for-legacy-camera-e.patch
-echo " "
-echo "Applying: frameworks: av: 009-PATCH-2-2-put-camera-and-omx-hals-into-one-process-l.patch"
-patch -d frameworks/av -p1 -N --no-backup-if-mismatch --reject-file=/tmp/rej < device/samsung/universal5420-common/patches/frameworks/av/009-PATCH-2-2-put-camera-and-omx-hals-into-one-process-l.patch
