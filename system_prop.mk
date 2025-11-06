da5fdb672b4eb56dae796d9b4cf0b6dc1ea94c9e
Salih Çağlar <salihcaglar4187@gmail.com>
2025-11-06 13:55:46 +0300

    Revert "Camera: Fix Camera recording"
    This reverts commit 4d1cc184068625be8f87c2b756616147e726eed2.

--- system_prop.mk
+++ system_prop.mk
@@ -65,6 +65,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true \
     debug.stagefright.ccodec=0 \
     debug.stagefright.omx_default_rank=0 \
     debug.stagefright.omx_default_rank.sw-audio=1
