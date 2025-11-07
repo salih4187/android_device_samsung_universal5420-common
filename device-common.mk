f6a577319abd6d547317b809e1a653ae60184b1d
Salih Çağlar <salihcaglar4187@gmail.com>
2025-11-07 08:57:44 +0300

    Revert "universal5420-common: Use default power hal"
    This reverts commit c552e242ff1ae2ec6532f50ebc1df98dcc749b67.

--- device-common.mk
+++ device-common.mk
@@ -145,12 +145,8 @@ PRODUCT_PACKAGES +=  \
     InProcessNetworkStack
 
 # Power
-#PRODUCT_PACKAGES += \
-#    android.hardware.power-service.exynos
-
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power-service.exynos
 
 # Permissions
 PRODUCT_COPY_FILES += \
