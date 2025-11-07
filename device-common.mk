2e76e84390b3e40912450db172c64a21f67070ed
Salih Çağlar <salihcaglar4187@gmail.com>
2025-11-07 11:17:12 +0300

    Revert "universal5420-common: Drop audio.a2dp.default"
    This reverts commit e176aa2c90ae54b46b8800f384e2775cccfd115c.
    
    Reason:
    11-07 11:11:55.524  3021  3058 E DevicesFactoryHAL: loadAudioInterface couldn't load audio hw module audio.a2dp (No such file or directory)
    11-07 11:11:55.524  3051  3051 W DevicesFactoryHalHidl: The specified device name is not recognized: "a2dp"
    11-07 11:11:55.524  3051  3051 E AudioFlinger: loadHwModule() error -22 loading module a2dp
    11-07 11:11:55.524  3051  3051 W APM_AudioPolicyManager: could not open HW module a2dp

--- device-common.mk
+++ device-common.mk
@@ -33,6 +33,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     audio.primary.universal5420 \
+    audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     tinymix \
