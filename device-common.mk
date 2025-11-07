e8204fc0321a0bc4f54ba612799c99af9ed779b8
Salih Çağlar <salihcaglar4187@gmail.com>
2025-11-07 11:29:54 +0300

    universal5420-common: Use Audio v4 HAL

--- device-common.mk
+++ device-common.mk
@@ -37,8 +37,8 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     tinymix \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
